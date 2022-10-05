aeb7700c7d33b4b4a56eae28deae81efd27625b2
helio <helio_ss_junior@hotmail.com>
2022-10-05 10:40:35 -0300

    Created Actions Results and methods

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 [Table("Produtos")]
@@ -28,5 +29,7 @@ public class Produto
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CateGoriaId { get; set; }
+
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
